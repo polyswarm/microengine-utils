b252e9447034b9ac5483a3926cf1cba4c4ef1a00
Rob Lathrop <rl@polyswarm.io>
2021-07-16 08:53:14 -0400

    Bump version: 1.4.1 → 1.5.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as readme:
 
 setup(
     name='microengine-utils',
-    version='1.4.1',
+    version='1.5.0',
     description='Library for Polyswarm Microengine Utility Package',
     long_description=long_description,
     long_description_content_type='text/markdown',
